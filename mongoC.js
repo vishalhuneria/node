f7e465f3d6d9bda38bf0eafa739e8d57678acc7f
vishalhuneria <vishalhuneria@gmail.com>
2025-07-08 01:12:45 +0530

    final i guess

--- mongoC.js
+++ mongoC.js
@@ -1,13 +1,16 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.hu5pjmi.mongodb.net/?retryWrites=true&w=majority`; // cluster url
+// const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.hu5pjmi.mongodb.net/?retryWrites=true&w=majority`; // cluster url
+const connectionString = `mongodb+srv://vishal:${password}@mernprojectcluster.ygj2gl8.mongodb.net/?retryWrites=true&w=majority&appName=mernprojectcluster`; // cluster url
+
+// mongodb+srv://vishal:${password}@mernprojectcluster.ygj2gl8.mongodb.net/?retryWrites=true&w=majority&appName=mernprojectcluster
 const client = new MongoClient(connectionString);
 let conn;
 try {
   conn = await client.connect();
-  console.log("connection successful")
-} catch(e) {
+  console.log("connection successful");
+} catch (e) {
   console.error(e);
 }
 let db = conn.db("integration_ninjas");
